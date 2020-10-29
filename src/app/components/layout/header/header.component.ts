990208a0b634e1c339212e45844f4b89b6df8ac1
Francisco José C. Silva <franzecs@gmail.com>
2020-10-29 06:21:58 -0300

    update ng 10.2.0

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -8,7 +8,7 @@ import { ResponseApi } from 'src/app/shared/model/util';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
